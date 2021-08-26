e27c38695d69886e5c13f67d1aba9402a91c92f9
Yerkanat <yerkanat.makhayev@gmail.com>
2021-08-26 14:28:18 +0600

    ssss

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const Pool = require('pg').Pool
 
 const pool = new Pool({
     user: 'dtvkjtio',
-    password: process.env.DB_PASSWORD,
+    password: 'Ea7jGySWS8PFtGtrhTDUk-6ekgc2jGLj',
     host: 'hattie.db.elephantsql.com',
     port: 5432,
     database: 'dtvkjtio'
