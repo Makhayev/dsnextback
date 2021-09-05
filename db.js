0bc242fa22cb11a1b83fe7ff30c8b5a1dd906650
Yerkanat <yerkanat.makhayev@gmail.com>
2021-09-05 22:49:25 +0600

    db connection fixed

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const pool = new Pool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST,
-    port: process.env.PORT,
+    port: process.env.DB_PORT,
     database: process.env.DB_DATABASE
 })
 
